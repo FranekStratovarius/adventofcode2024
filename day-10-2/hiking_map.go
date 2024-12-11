47b3ed9ebf2fbad632c19b71e7130d6d29a938a3
FranekStratovarius <louishuss2000ggmail.com>
2024-12-11 03:50:13 +0100

    added day 10

---
+++ day-10-2/hiking_map.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"github.com/sirupsen/logrus"
+)
+
+type HikingMap struct {
+	heightmap    [][]int
+	hikingTrails []HikingTrail
+}
+
+type Position struct {
+	x int
+	y int
+}
+
+type HikingTrail struct {
+	positions []Position
+}
+
+func (hikingMap *HikingMap) addRow(line string) {
+	var row []int
+	for _, tile := range line {
+		row = append(row, int(tile-'0'))
+	}
+	hikingMap.heightmap = append(hikingMap.heightmap, row)
+}
+
+func (hikingMap HikingMap) print() {
+	for _, row := range hikingMap.heightmap {
+		for _, tile := range row {
+			logrus.Debugf("%d", tile)
+		}
+		logrus.Debugf("\n")
+	}
+}
+
+func (hikingMap *HikingMap) findHikingTrails() (trailheadRatingSum int) {
+	for y, row := range hikingMap.heightmap {
+		for x, tile := range row {
+			if tile == 0 {
+				var hikingTrails []HikingTrail
+				hikingMap.findHikingTrailsRecursive(
+					&hikingTrails,
+					HikingTrail{},
+					Position{
+						x: x,
+						y: y,
+					},
+				)
+				// logrus.Debugf("\n")
+				// logrus.Debugf("trailheads: %v\n", trailheads)
+				trailheadRatingSum += len(hikingTrails)
+			}
+		}
+	}
+	return
+}
+
+func (hikingMap *HikingMap) findHikingTrailsRecursive(hikingTrails *[]HikingTrail, hikingTrail HikingTrail, newPosition Position) {
+	// deep copy positions
+	newPositions := make([]Position, len(hikingTrail.positions))
+	copy(newPositions, hikingTrail.positions)
+	newPositions = append(newPositions, newPosition)
+
+	// set positions of hiking trail
+	hikingTrail.positions = newPositions
+
+	height := len(hikingTrail.positions) - 1
+
+	// if position is outside of map, search has failed
+	if newPosition.y < 0 ||
+		newPosition.x < 0 ||
+		newPosition.y >= len(hikingMap.heightmap) ||
+		newPosition.x >= len(hikingMap.heightmap[0]) {
+		// logrus.Debugf("outside\n")
+		return
+	}
+
+	// stop if current height is not the desired height
+	if hikingMap.heightmap[newPosition.y][newPosition.x] != height {
+		return
+	}
+
+	// if height 9 is reached, search is successful
+	if height == 9 {
+		// fmt.Printf("finished: %+v\n", hikingTrail.positions)
+		*hikingTrails = append(*hikingTrails, hikingTrail)
+		return
+	}
+
+	// check in each direction
+	positionUp := newPosition
+	positionUp.y++
+	// hikingMap.findHikingTrailsRecursive(hikingTrails, hikingTrail, Position{x: newPosition.x, y: newPosition.y + 1})
+	hikingMap.findHikingTrailsRecursive(hikingTrails, hikingTrail, positionUp)
+	positionRight := newPosition
+	positionRight.x++
+	hikingMap.findHikingTrailsRecursive(hikingTrails, hikingTrail, positionRight)
+	positionDown := newPosition
+	positionDown.y--
+	hikingMap.findHikingTrailsRecursive(hikingTrails, hikingTrail, positionDown)
+	positionLeft := newPosition
+	positionLeft.x--
+	hikingMap.findHikingTrailsRecursive(hikingTrails, hikingTrail, positionLeft)
+}
---
+++ day-10-2/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+type PlainFormatter struct{}
+
+func (formatter *PlainFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+	return []byte(fmt.Sprintf(entry.Message)), nil
+}
+
+func main() {
+	logrus.SetFormatter(&PlainFormatter{})
+	logrus.SetLevel(logrus.DebugLevel)
+	scanner := bufio.NewScanner(os.Stdin)
+
+	var hikingMap HikingMap
+	for scanner.Scan() {
+		line := scanner.Text()
+		// logrus.Debugf("> %s\n", line)
+		hikingMap.addRow(line)
+	}
+
+	// hikingMap.print()
+
+	trailheadRatingSum := hikingMap.findHikingTrails()
+
+	fmt.Printf("summed ratings of trailheads: %d\n", trailheadRatingSum)
+}
